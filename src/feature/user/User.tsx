1a6fef478cba0a6d35e35e5237fbdfefc79e2697
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-02-01 20:21:12 +0530

    added the typescript file

--- src/feature/user/User.tsx
+++ src/feature/user/User.tsx
@@ -1,10 +1,9 @@
- 
 const User = () => {
   return (
     <>
-      <h1>Hello world from User</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
-  )
-}
+  );
+};
 
-export default User
+export default User;
